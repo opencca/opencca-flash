b40d72f5a61b93a43b429235f5a4ad9657c54eb0
Andrin Bertschi <hi@abertschi.ch>
2025-01-27 13:51:18 +0100

    opencca: source venv if it exists

--- board/maskrom.sh
+++ board/maskrom.sh
@@ -2,7 +2,16 @@
 set -euo pipefail
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-set -x
+VENV_DIR=$SCRIPT_DIR/venv
+
+
+if [ ! -d "$VENV_DIR" ]; then
+    echo "Error: 'venv' folder does not exist in $SCRIPT_DIR."
+    echo "Please create a virtual environment first."
+    exit 1
+fi
+
+source "$VENV_DIR/bin/activate"
 
 $SCRIPT_DIR/power.sh off
 python $SCRIPT_DIR/gpio-maskrom.py &
