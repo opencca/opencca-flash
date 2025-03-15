a482f79cb94925a835e73fc6bd6a523715e324cd
Andrin Bertschi <hi@abertschi.ch>
2025-03-15 20:18:40 +0100

    opencca: move power functionality to power.sh

--- flash/board/board.sh
+++ flash/board/board.sh
@@ -9,30 +9,22 @@ readonly SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null 
 readonly BOARD_POWER="$SCRIPT_DIR/power/power.sh"
 readonly BOARD_MASKROM="$SCRIPT_DIR/maskrom/maskrom.sh"
 
-function board_off { bash "$BOARD_POWER" off; }
-function board_on { bash "$BOARD_POWER" on;  }
-
-# XXX: In case device does not reboot but remains in
-#      maskrom mode than sleep between off and on is too little
-function board_reboot { echo "rebooting"; board_off; sleep 3; board_on; }
+function board_power { bash "$BOARD_POWER" $1; }
 
 function run_maskrom_sequence() {
-    board_off
-
     echo "Pressing Maskrom button..."
     bash "$BOARD_MASKROM" press
-    sleep 2
-    board_on
-    sleep 5
+
+    board_power reboot
+
+    sleep 7 
     bash "$BOARD_MASKROM" release
 }
 
 function board_control() {
     set +u
     case "$1" in
-        on) board_on ;;
-        off) board_off ;;
-        reboot) board_reboot ;;
+        off|on|reboot) board_power $1 ;;
         maskrom) run_maskrom_sequence ;;
         *) echo "Usage: $0 {on|off|reboot|maskrom}" && exit 1 ;;
     esac
